41e2de2789b7b78ef1d9e52e22d32adb82958e8c
ALBERT SJÖLUND <al5518sj-s@student.lu.se>
2019-12-12 19:36:37 +0100

    Ticket example!

---
+++ examples/ticket/ticket_test.go
@@ -0,0 +1,106 @@
+package gen
+
+import (
+	"context"
+	"fmt"
+	"github.com/Vliro/humus"
+	"github.com/dgraph-io/dgo/protos/api"
+	"io/ioutil"
+	"testing"
+)
+
+func TestTicketQuery(t *testing.T) {
+	ev, err := NewEvent("Event", "Best event", []int{1, 3, 5})
+	if err != nil {
+		t.Fail()
+		return
+	}
+	var q = humus.NewQuery(eventFields).Function(humus.FunctionUid).Values(ev.Uid)
+
+	q.At("", func(m humus.Mod) {
+		m.Sort(humus.Descending, EventNameField)
+	})
+
+	str, _ := q.Process()
+	fmt.Println(str)
+}
+
+func TestTicket(t *testing.T) {
+	ev, err := NewEvent("Event", "Best event", []int{1, 3, 5})
+	if err != nil {
+		t.Fail()
+		return
+	}
+	us, err := NewUser("User")
+	if err != nil {
+		t.Fail()
+		return
+	}
+	uss, err := NewUser("Another user")
+	if err != nil {
+		t.Fail()
+		return
+	}
+	ok := AddUserToEvent(ev.Uid, us.Uid, 1)
+	ok = AddUserToEvent(ev.Uid, uss.Uid, 2)
+	if !ok {
+		t.Fail()
+		return
+	}
+	//Returns an event with attending users ordered by the premium facet.
+	ev, err = EventOrderByPremium(ev.Uid)
+	if err != nil {
+		t.Fail()
+		return
+	}
+	_, i := AttendingEvent(ev.Uid)
+	if i != 2 {
+		t.Fail()
+	}
+	fmt.Println(i)
+}
+
+func TestMain(m *testing.M) {
+	conf := &humus.Config{
+		IP:         "localhost",
+		Port:       9080,
+		Tls:        false,
+		RootCA:     "",
+		NodeCRT:    "",
+		NodeKey:    "",
+		LogQueries: true,
+	}
+	db = humus.Init(conf, GetGlobalFields())
+	err := db.Alter(context.Background(), &api.Operation{
+		DropAll: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+	sch, err := ioutil.ReadFile("schema.txt")
+	if err != nil {
+		panic(err)
+	}
+	err = db.Alter(context.Background(), &api.Operation{
+		Schema: string(sch),
+	})
+	if err != nil {
+		panic(err)
+	}
+	m.Run()
+}
+
+func TestFacets(t *testing.T) {
+	var q = humus.NewQuery(eventFields).Function(humus.Type).Values("Event")
+	q.Facets(EventAttendingField, func(m humus.Mod) {
+		m.Variable("result", "premium", false)
+		m.Sort(humus.Descending, "premium")
+	})
+	q.At("", func(m humus.Mod) {
+		m.Aggregate(humus.Sum, "result", "sum")
+		m.Sort(humus.Descending, EventNameField)
+		m.Paginate(humus.CountFirst, 5)
+	})
+	str, _ := q.Process()
+	fmt.Println(str)
+}
